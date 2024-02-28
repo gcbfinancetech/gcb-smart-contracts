9429926af21b72d750c9b1b04115c53cd08834a5
gcbdev <ceo@gulfcb.io>
2024-02-28 11:37:47 +0300

    tron contract flattened

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gcb-smart-contracts
 GCB Smart Contracts
+
+
+## Flatting the contracts
+```
+npx hardhat flatten contracts/GCB_Token_Tron.sol > contracts/GCB_Tron_Flatten.sol
+```
--- contracts/GCB_Tron_Flatten.sol
+++ contracts/GCB_Tron_Flatten.sol
@@ -2314,20 +2314,4 @@ contract GCB is ERC20, ERC20Burnable, ERC20Permit, Ownable {
     {
         _mint(msg.sender, initialSupply * 10 ** decimals());
     }
-
-    function rescueEth() external onlyOwner {
-        // Transfer the ETH in the contract to the owner's account
-        payable(msg.sender).transfer(address(this).balance);
-    }
-
-    function rescueERC20(address tokenAddress) external onlyOwner {
-        // Use the interface of the ERC-20 token to interact with it
-        IERC20 token = IERC20(tokenAddress);
-        
-        // Get the balance of the token in the contract
-        uint256 balance = token.balanceOf(address(this));
-
-        // Transfer the token to the admin's account
-        require(token.transfer(msg.sender, balance), "Transfer failed");
-    }
 }
